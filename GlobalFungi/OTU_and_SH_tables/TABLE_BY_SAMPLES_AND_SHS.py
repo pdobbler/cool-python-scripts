125beae9dc1f2c2958ca12113629ab9dd7919f8b
Tomáš Větrovský <vetrovsky@biomed.cas.cz>
2024-03-11 17:49:17 +0100

    Add files via upload

---
+++ GlobalFungi/OTU_and_SH_tables/TABLE_BY_SAMPLES_AND_SHS.py
@@ -0,0 +1,126 @@
+__author__ = 'vetrot'
+
+import sys
+import os
+import hashlib
+import gzip
+import csv
+from cStringIO import StringIO  # Python 2
+
+sh_tab = sys.argv[1]            # GF5_RAW_TABLE_TAB_SH.txt.gz
+sample_pairing = sys.argv[2]    # GF5_RAW_TABLE_SAMPLES.txt.gz
+sample_list = sys.argv[3]       # test_samples.txt OR -
+shs_list = sys.argv[4]          # test_shs.txt OR -
+
+#############################################
+# GZIP OPENING
+#############################################
+def openfile(filename, mode='r'):
+    if filename.endswith('.gz'):
+        return gzip.open(filename, mode)
+    else:
+        return open(filename, mode)
+
+
+#############################################
+# LOAD PAIRS - sampleID+permanentID 
+#############################################
+# 75387   GF04000029S
+sample_pairs = {}
+for line in openfile(sample_pairing):
+     vals = line.rstrip().split('\t')
+     sample_pairs[vals[0]] = vals[1]
+print("Samples paired - " + str(len(sample_pairs)))
+
+
+#############################################
+# LOAD SAMPLES
+#############################################
+#print("Sample file: " + sample_list)
+
+samples = {}
+s_list = []
+i = 0
+if sample_list == "-":
+    for sid in sample_pairs:
+        samples[sample_pairs[sid]] = i
+        s_list.append(sample_pairs[sid])
+        i += 1
+else:
+    for line in openfile(sample_list):
+        samples[line.rstrip()] = i
+        if not line.rstrip() == "":
+            s_list.append(line.rstrip())
+            i += 1
+
+print("Samples loaded - " + str(len(samples)))
+
+
+#############################################
+# LOAD SHs
+#############################################
+#print("SHs file: " + shs_list)
+
+shs = {}
+allSHs = False
+i = 0
+if shs_list == "-":
+    allSHs = True
+    print("SHs all")
+else:
+    for line in openfile(shs_list):
+        if not line.rstrip() == "":
+            shs[line.rstrip()] = i
+            i += 1
+    print("SHs loaded - " + str(len(shs)))
+
+
+#############################################
+# EXTRACT ABUNDANCES
+#############################################
+tab_data = {}
+h_list = []
+for line in openfile(sh_tab):
+        l = line.rstrip()
+        vals = l.split('\t')
+        sh = vals[0]
+        sn = vals[1].split(';') #sample names
+        sa = vals[2].split(';') #sample abundances
+        # precheck
+        if allSHs or shs.has_key(sh):
+            h_list.append(sh)
+            abund = {}
+            for x in range(len(sn)):
+                sample = sample_pairs[sn[x]]
+                if samples.has_key(sample):
+                    ab = int(sa[x])
+                    abund[sample] = ab
+            tab_data[sh] = abund
+
+
+#############################################
+# SAVE OUTPUT
+#############################################
+# save it
+fp = open("OUTPUT_TAB.txt", "w")
+
+# header
+line = "SH_name"
+for sample in s_list:
+    line += "\t" + sample
+fp.write(line + "\n")
+
+# body
+for sh in h_list:
+    line = sh
+    abund = tab_data[sh]
+    for sample in s_list:
+        if abund.has_key(sample):
+            line += "\t" + str(abund[sample])
+        else:
+            line += "\t0"
+    fp.write(line + "\n")
+ 
+fp.close()
+
+print("DONE :]")
