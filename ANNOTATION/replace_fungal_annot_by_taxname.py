33234990eaae04e29e752b8040d856f930ec5a2c
Tomáš Větrovský <vetrovsky@biomed.cas.cz>
2024-04-10 18:44:20 +0200

    Add files via upload

---
+++ ANNOTATION/replace_fungal_annot_by_taxname.py
@@ -0,0 +1,41 @@
+__author__ = 'vetrot'
+
+import sys
+import os
+
+annotation = sys.argv[1]
+short_to_tax = sys.argv[2]
+output_file = sys.argv[3]
+
+#fwd_k141_10000352_1_282_-       jgi|Dacma1|778102|KOG1368|4.1.2.5       52.5    80      38      0       13      92      20      99      5.1e-15 87.0
+
+names = {}
+for line in open(short_to_tax):
+    vals = line.rstrip().split('\t')
+    names[vals[0]] = vals[1]
+
+print('Taxon paires were loaded - '+str(len(names)))
+
+fp = open(output_file, "w")
+for line in open(annotation):
+    vals = line.rstrip().split('\t')
+    new_line = vals[0]
+    for i in range(1,len(vals)):
+        if i == 1:
+            tax = vals[i].split('|')[1]
+            if names.has_key('['+tax+']'):
+                new_line = new_line + '\t' + names['['+tax+']']
+            else:
+                print('Taxa abreciation was not found - '+tax)
+                new_line = new_line + '\t' + vals[i]
+        else:
+            new_line = new_line + '\t' + vals[i]
+    #print(new_line)
+    fp.write(new_line + '\n')
+
+fp.close()
+
+print('Done :]')
+
+
+
