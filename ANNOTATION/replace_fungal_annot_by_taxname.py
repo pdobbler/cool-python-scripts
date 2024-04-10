6e1e51da0d1fc0ebc155079190ad595e8ef8e3d3
Tomáš Větrovský <vetrovsky@biomed.cas.cz>
2024-04-10 18:59:50 +0200

    Add files via upload

--- ANNOTATION/replace_fungal_annot_by_taxname.py
+++ ANNOTATION/replace_fungal_annot_by_taxname.py
@@ -6,6 +6,12 @@ import os
 annotation = sys.argv[1]
 short_to_tax = sys.argv[2]
 output_file = sys.argv[3]
+outtype = sys.argv[4]           # D/
+
+if outtype == 'D':
+    print("Out-type is: DEFAULT")
+if outtype == 'U':
+    print("Out-type is: UPDATE")
 
 #fwd_k141_10000352_1_282_-       jgi|Dacma1|778102|KOG1368|4.1.2.5       52.5    80      38      0       13      92      20      99      5.1e-15 87.0
 
@@ -28,6 +34,8 @@ for line in open(annotation):
             else:
                 print('Taxa abreciation was not found - '+tax)
                 new_line = new_line + '\t' + vals[i]
+            if outtype == U:
+                new_line += '|' + vals[i].split('|')[3]
         else:
             new_line = new_line + '\t' + vals[i]
     #print(new_line)
