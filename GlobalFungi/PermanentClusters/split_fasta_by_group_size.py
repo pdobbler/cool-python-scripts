da8fd80ebc5a1e16c6cb34b35a36b4f51614252c
Tomáš Větrovský <vetrovsky@biomed.cas.cz>
2024-09-16 09:37:13 +0200

    Add files via upload

--- GlobalFungi/PermanentClusters/split_fasta_by_group_size.py
+++ GlobalFungi/PermanentClusters/split_fasta_by_group_size.py
@@ -2,9 +2,21 @@ __author__ = 'vetrot'
 
 import sys
 import ntpath
+import gzip
 
 in_file = sys.argv[1]
 group_size = int(sys.argv[2])
+
+#############################################
+# GZIP OPENING
+#############################################
+def openfile(filename, mode='r'):
+    if filename.endswith('.gz'):
+        return gzip.open(filename, mode)
+    else:
+        return open(filename, mode)
+
+#############################################
 
 part = 1
 k = 1
@@ -12,7 +24,7 @@ k = 1
 file_name = ntpath.basename(in_file).rstrip().split('.')[0]
 fp = open(file_name+str(part)+'.fas', 'w')
 
-for n, line in enumerate(open(in_file)):
+for n, line in enumerate(openfile(in_file)):
     ch = line[0]
     if ch == '>':
         if ((k % group_size)==0):
