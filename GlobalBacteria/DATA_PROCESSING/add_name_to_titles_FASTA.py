436a3f01977abf230e4a3268eeb3675bedae6583
Tomáš Větrovský <vetrovsky@biomed.cas.cz>
2025-03-04 09:26:19 +0100

    Add files via upload

---
+++ GlobalBacteria/DATA_PROCESSING/add_name_to_titles_FASTA.py
@@ -0,0 +1,40 @@
+__author__ = 'vetrot'
+
+import sys
+import os
+
+fastq_file = sys.argv[1]
+sample_name = sys.argv[2]
+
+print("name to add to the end of tiitles: " + sample_name)
+
+
+############################################################
+# open gzip
+############################################################
+
+def openfile(filename, mode='r'):
+    if filename.endswith('.gz'):
+        return gzip.open(filename, mode)
+    else:
+        return open(filename, mode)
+
+############################################################
+
+
+fp = open(fasta_file+".renamed", 'w')
+for line in openfile(fasta_file, 'r'):
+    ch = line[0]
+    if ch == '>':
+        titleRead = True
+        title = line[1:].strip()
+    else:
+        if titleRead:
+            titleRead = False
+            seq = line.strip()
+            fp.write(">" + sample_name + '|' + title + "\n")
+            fp.write(seq + "\n")
+fp.close()
+
+
+print("Done :]")
