36b9b8772fd959588a4b33f36573dc43c0e71944
Tomáš Větrovský <vetrovsky@biomed.cas.cz>
2025-03-07 12:47:43 +0100

    Add files via upload

--- GlobalBacteria/DATA_PROCESSING/search_for_primary_motive_with_reverse.py
+++ GlobalBacteria/DATA_PROCESSING/search_for_primary_motive_with_reverse.py
@@ -19,12 +19,12 @@ def compl_nucl(x):
         'T': 'A',
         'C': 'G',
         'G': 'C'
-    }.get(x, 'N')    # 9 is default if x not found
+    }.get(x, 'N')    # 'N' is default if x not found
 
 def make_revcompl(seq):
     rev_compl_seq = ''
-    for i in range(0,len(seq)):
-        rev_compl_seq=rev_compl_seq+compl_nucl(seq[len(seq)-(i+1)].upper())
+    for i in range(len(seq)):
+        rev_compl_seq = rev_compl_seq + compl_nucl(seq[len(seq) - (i + 1)].upper())
     return rev_compl_seq
 
 # loading of barcodes...
@@ -32,13 +32,13 @@ print("motive " + motive + " mismatch allowed " + str(mismatches))
 
 def compareStrings(s1, s2):
     mm = 0
-    for i in range(0, len(s1)):
-        if not s1[i] == s2[i]:
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
             mm += 1
     return mm
 
 def search(sequence, adapt, mismatch):
-    for i in range(0, len(sequence)):
+    for i in range(len(sequence)):
         part = sequence[i:]
         if len(part) < len(adapt):
             break
@@ -54,35 +54,36 @@ count = 0
 fwd_count = 0
 rev_count = 0
 no_hit = 0
-fp = open(FASTA + ".PRIMARY.fa", 'w')
-for n, line in enumerate(openfile(FASTA)):
-    if n % 2 == 0:
-        r1_0 = line.rstrip()
-    else:
-        if n % 2 == 1:
-            r1_1 = line.rstrip()
-            filled = True
-    if filled:
-        filled = False
-        count += 1
-        # search for motives
-        title = r1_0.split(' ')[0]
-        seq = r1_1
-        pos = search(seq, motive, mismatches)
-        if pos > -1:
-            fp.write(title + '|POS=' + str(pos) + '\n')
-            fp.write(seq + '\n')
-            fwd_count += 1
-            continue
+
+# Open output file in gzip mode
+with gzip.open(FASTA + ".PRIMARY.fa.gz", 'wt') as fp:
+    for n, line in enumerate(openfile(FASTA)):
+        if n % 2 == 0:
+            r1_0 = line.rstrip()
         else:
-            seq = make_revcompl(seq)
+            if n % 2 == 1:
+                r1_1 = line.rstrip()
+                filled = True
+        if filled:
+            filled = False
+            count += 1
+            # search for motives
+            title = r1_0.split(' ')[0]
+            seq = r1_1
             pos = search(seq, motive, mismatches)
             if pos > -1:
-                fp.write(title + '|POS=' +str(pos) + '\n')
+                fp.write(title + '|POS=' + str(pos) + '\n')
                 fp.write(seq + '\n')
-                rev_count += 1
+                fwd_count += 1
                 continue
-        no_hit += 1
-fp.close()
+            else:
+                seq = make_revcompl(seq)
+                pos = search(seq, motive, mismatches)
+                if pos > -1:
+                    fp.write(title + '|POS=' + str(pos) + '\n')
+                    fp.write(seq + '\n')
+                    rev_count += 1
+                    continue
+            no_hit += 1
 
-print("Total seqs: "+str(count)+" Total found: "+str(fwd_count+rev_count)+" (FWD: "+str(fwd_count)+"/REV: "+str(rev_count)+") NO HIT: "+str(no_hit))
+print("Total seqs: " + str(count) + " Total found: " + str(fwd_count + rev_count) + " (FWD: " + str(fwd_count) + "/REV: " + str(rev_count) + ") NO HIT: " + str(no_hit))
