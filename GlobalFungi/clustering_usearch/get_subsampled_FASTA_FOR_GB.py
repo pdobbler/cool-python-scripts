b253e3bcdcd56bf239da01352d994c1d262bfec7
Tomáš Větrovský <vetrovsky@biomed.cas.cz>
2025-04-02 10:04:46 +0200

    Add files via upload

---
+++ GlobalFungi/clustering_usearch/get_subsampled_FASTA_FOR_GB.py
@@ -0,0 +1,55 @@
+import sys
+import random
+import gzip
+from collections import defaultdict
+
+# Inputs
+size_discard = int(sys.argv[1])   # number of sequences per sample 12000
+fasta_file = sys.argv[2]          # input fasta file
+output_file = sys.argv[3]         # output file for selected sequences
+discarded_file = sys.argv[4]      # file to save names of discarded samples
+size_retain = int(sys.argv[5])    # number of sequences per sample 15000
+
+# open gzip files
+def openfile(filename, mode='r'):
+    if filename.endswith('.gz'):
+        return gzip.open(filename, mode + 't')
+    else:
+        return open(filename, mode)
+
+# Read all sequences and group them by sample
+samples = defaultdict(list)
+with openfile(fasta_file) as f:
+    while True:
+        header = f.readline()
+        if not header:
+            break
+        seq = f.readline()
+        if not seq:
+            break
+        if header.startswith('>'):
+            sample_name = header.split('|')[0][1:]  # Get 'GB01001571S' from '>GB01001571S|...'
+            samples[sample_name].append((header.strip(), seq.strip()))
+
+# Select and save random sequences
+discarded_samples = []
+with open(output_file, 'w') as out:
+    for sample, seqs in samples.items():
+        if len(seqs) >= size_discard:
+            if len(seqs) <= size_retain:
+                for header, sequence in seqs:
+                    out.write(f"{header}\n{sequence}\n")
+            else:
+                selected = random.sample(seqs, size_retain)
+                for header, sequence in selected:
+                    out.write(f"{header}\n{sequence}\n")
+        else:
+            discarded_samples.append(sample)
+
+# Save discarded sample names
+with open(discarded_file, 'w') as dfile:
+    for sample in discarded_samples:
+        dfile.write(sample + "\n")
+
+print(f"Done. Selected sequences written to {output_file}")
+print(f"Discarded samples written to {discarded_file}")
