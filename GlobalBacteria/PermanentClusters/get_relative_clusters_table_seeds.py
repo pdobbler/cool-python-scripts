72c9d338d6f4cf559f943406838b59d9ab59bde1
Tomáš Větrovský <vetrovsky@biomed.cas.cz>
2025-11-04 11:53:24 +0100

    Add files via upload

---
+++ GlobalBacteria/PermanentClusters/get_relative_clusters_table_seeds.py
@@ -0,0 +1,121 @@
+__author__ = 'vetrot'
+
+import sys
+import operator
+import collections
+import gzip
+import hashlib
+
+samplevar_file = sys.argv[1] # VARIANTS_samplevar.txt.gz
+sample_pairs = sys.argv[2]   # VARIANTS_TABLE_SAMPLE_PAIRS.txt
+cl_max = int(sys.argv[3])    # 100000
+tax_file = sys.argv[4]    # TAXONOMY_CLUSTERS.sorted.txt
+vars_file = sys.argv[5]    # VARIANTS_variants.txt.gz
+
+def openfile(filename, mode='r'):
+    if filename.endswith('.gz'):
+        return gzip.open(filename, mode)
+    else:
+        return open(filename, mode)
+
+def make_cluster_name(num):
+    """
+    Generate cluster name in the format 'GB00000001.1'
+    from a given integer number.
+    Works in Python 2.7.
+    """
+    return "GB{:08d}.1".format(num)
+
+# get seeds md5
+# 3       GB00000003.1    -       -       100.0   100.0   d__Bacteria; p__Cyanobacteriota; c__Chloroplast; o__; f__; g__; s__     7911816f5e81f650f769aba0d5c708cb
+# 4       GB00000004.1    -       Variibacter     100.0   100.0   d__Bacteria; p__Pseudomonadota; c__Alphaproteobacteria; o__Rhizobiales_505101; f__Xanthobacteraceae; g__Variibacter; s__        90aca794c7e30b8a77e87f13ffc9a5cc
+cl_seeds_md5 = {}
+for line in openfile(tax_file):
+    vals = line.rstrip().split('\t')
+    md5 = vals[7]
+    cl_id = vals[0]
+    cl_seeds_md5[md5] = cl_id
+
+# get var_id for clusters - var_id to cl_id
+# 1       7933    7b26301f5893e689b354a0a981910c02        TACGGAGGGTGCAATCGTTATTCGGATTTACTGGGCTTAAAGCGCGCGTAGGCGGCCGTTTAAGTCAGATGTGAAAGCCCGGGGCTCAACCCTGGAAGTGCATTTGATACTATTCGGCTTGAGTATGGGAGAGGGAAGTGGAATTCCTGGTGTAGAGGTGAAATTCGTAGATATCAGGAGGAACACCGGTGGCGAAGGCGACTTCCTGGACCAATACTGACGCTGAGGCGCGAAGGCGTGGGGAGCAAACAGG
+# 2       375     99a556f970834de4c7d9eda6d9a4b423        GACGA
+cl_vars = {}
+for line in openfile(tax_file):
+    vals = line.rstrip().split('\t')
+    md5 = vals[2]
+    var_id = vals[0]
+    cl_vars[var_id] = cl_seeds_md5[md5]
+
+# SampleName      SampleID        occurence
+# 16349   GB01007340S     5521
+# 9601    GB01012613S     19312
+
+samples = {}
+samples_occ = {}
+for n, line in enumerate(openfile(sample_pairs)):
+    if n > 0:
+        vals = line.rstrip().split('\t')
+        samples[vals[0]] = vals[1]
+        samples_occ[vals[1]] = float(vals[2])
+
+
+# id      var_id  sam_id  sam_ab  cl_id
+# 18      13      96      1       195
+# 19      13      9727    1       195
+# 20      13      9214    1       195
+# 21      14      2076    1       1000000000000
+# 22      15      454     1       281
+# 23      16      1167    1       6346
+# fp_samplevar.write(str(sv_id) + '\t' + str(var_id) + '\t' + sn[x] + '\t' + sa[x] + '\t' + cl_id + '\n')
+
+cl_samples = {}
+sample_names = set()
+for line in openfile(samplevar_file):
+    vals = line.rstrip().split('\t')
+    cl_id = int(vals[4])
+    var_id = vals[1]
+    if cl_id <= cl_max:
+        if cl_vars.has_key(var_id):
+            sam_name = samples[vals[2]]
+            sam_ab = int(vals[3])
+            sample_names.add(sam_name)
+            if cl_samples.has_key(cl_id):
+                sample_ab = cl_samples[cl_id]
+                if sample_ab.has_key(sam_name):
+                    sample_ab[sam_name] += sam_ab
+                else:
+                    sample_ab[sam_name] = sam_ab
+                cl_samples[cl_id] = sample_ab
+            else:
+                sample_ab = {}
+                sample_ab[sam_name] = sam_ab
+                cl_samples[cl_id] = sample_ab
+
+print("clusters: "+str(len(cl_samples)))
+
+sample_list = []
+for sam_name in sample_names:
+    sample_list.append(sam_name)
+
+print("sample names: "+str(len(sample_list)))
+
+fp = open("OTUTABLE_TOP_CLUSTERS_"+str(cl_max)+".txt", 'w')
+line = "GB_CLUSTER"
+for sam_name in sample_list:
+    line += "\t" + sam_name
+fp.write(line +'\n')
+for i in range(1, cl_max + 1):
+    sample_ab = cl_samples[i]
+    line = make_cluster_name(i)
+    for sam_name in sample_list:
+        if sample_ab.has_key(sam_name):
+            line += "\t" + str(float(sample_ab[sam_name])/samples_occ[sam_name]*100)
+        else:
+            line += "\t0"
+    fp.write(line +'\n')
+fp.close()
+
+print("Done :)")
+
+
+
