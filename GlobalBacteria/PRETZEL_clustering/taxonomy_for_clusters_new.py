69c39b550ce48c26c11d6ce79f9bf8286391fee6
Tomáš Větrovský <vetrovsky@biomed.cas.cz>
2026-05-25 16:35:56 +0200

    Add files via upload

--- GlobalBacteria/PRETZEL_clustering/taxonomy_for_clusters_new.py
+++ GlobalBacteria/PRETZEL_clustering/taxonomy_for_clusters_new.py
@@ -5,6 +5,7 @@ import os
 import gzip
 
 taxonomy_ident = sys.argv[1]        # CLUSTERS_INFO_TOTAL_FINAL.txt
+greengenes2_tax = sys.argv[2]       # 2024.09.taxonomy.id.tsv.gz
 
 thr_s = 197.0
 thr_g = 190.0
@@ -14,6 +15,14 @@ def openfile(filename, mode='r'):
         return gzip.open(filename, mode)
     else:
         return open(filename, mode)
+
+#Feature ID      Taxon   Confidence
+# GB-GCA-003568775.1-MWMI01000008.1       d__Bacteria; p__; c__; o__; f__; g__; s__       0.3
+hits = {}
+for line in openfile(greengenes2_tax):
+    parts = line.rstrip().split("\t")
+    hits[parts[0]] = parts[1]
+
 
 # clName  md5     totSeqSize      totRepSize      HIT     TAXONOMY        SIMILARITY      COVERAGE        EVALUE  BITSCORE        SEQ     totalSeqs       totalSamples    totalStudies    repSamples      repStudies      clusterRelAbund_sum     clusterVariants clusterVariantSize_sum
 # CL00000001      6472eb8b1e09f892aca2f23182962903        27490562        6712798 GB-GCA-004799405.1-SSMW01000135.1       d__Bacteria;p__Pseudomonadota;c__Alphaproteobacteria;o__Rhizobiales_505101;f__Xanthobacteraceae;g__Bradyrhizobium_503372;s__Bradyrhizobiumsp000244915   100.000 100.0   1.18e-130       468     TACGAAGGGGGCTAGCGTTGCTCGGAATCACTGGGCGTAAAGGGTGCGTAGGCGGGTCTTTAAGTCAGGGGTGAAATCCTGGAGCTCAACTCCAGAACTGCCTTTGATACTGAGGATCTTGAGTTCGGGAGAGGTGAGTGGAACTGCGAGTGTAGAGGTGAAATTCGTAGATATTCGCAAGAACACCAGTGGCGAAGGCGGCTCACTGGCCCGATACTGACGCTGAGGCACGAAAGCGTGGGGAGCAAACAGG   27490562        28379   169     23821   164     448.303121968   2994881 27490562
@@ -27,27 +36,28 @@ for line in openfile(taxonomy_ident):
     line = line.rstrip()
     if i > 0:
         parts = line.split("\t")
-        taxons = parts[5]
+        taxons = parts[4]
         md5 = parts[1]
         cl = parts[0]
         genus = '-'
         species = '-'
-        if taxons == 'UNKNOWN':
+        if taxons == 'NO_HIT':
             taxons = 'unidentified'
         else:
+            taxons = hits[taxons]
             sim = float(parts[6])
             cov = float(parts[7])
             if sim + cov >= thr_g:
-                gen = taxons.split(';')[5].replace("g__", "")
+                gen = taxons.split(';')[5].replace(" g__", "")
                 if not gen == '':
                     genus = gen
                     gen_taxonomy[genus] = taxons
                 if sim + cov >= thr_s:
-                    sp = taxons.split(';')[6].replace("s__", "")
+                    sp = taxons.split(';')[6].replace(" s__", "")
                     if not sp == '':
                         species = sp
                         sp_taxonomy[species] = taxons
-        fp.write(cl + '\t' + species + '\t' + genus + '\t' + str(sim) + '\t' + str(cov) + '\t' + taxons.replace(";", "; ") + '\t' + md5 + '\n')
+        fp.write(cl + '\t' + species + '\t' + genus + '\t' + str(sim) + '\t' + str(cov) + '\t' + taxons + '\t' + md5 + '\n')
     i += 1
 fp.close()
 
@@ -56,7 +66,7 @@ print("Taxonomy loaded: " + str(i) + " - the size should be the same as clusters
 fp = open("TAXONOMY_SP_GEN.txt", 'w')
 for species in sp_taxonomy:
     taxons = sp_taxonomy[species]
-    gen = taxons.split(';')[5].replace("g__", "")
+    gen = taxons.split(';')[5].replace(" g__", "")
     if gen_taxonomy.has_key(gen):
         del gen_taxonomy[gen]
     fp.write('sp\t' + species + '\t' + taxons + '\n')
